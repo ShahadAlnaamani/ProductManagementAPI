228dd85e2b967f4c1029ebafcb151cd7aa4ecd00
Shahad <Shahadalnaamani@outlook.com>
2024-12-12 12:42:09 +0400

    Created model and linked to DbContext

---
+++ Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductManagementAPI.Models
+{
+    public class Product
+    {
+        [Key]
+        public int PID { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+
+        [MaxLength(50)]
+        public string Category { get; set; }
+
+        public DateTime DateAdded { get; set; }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 
+using Microsoft.EntityFrameworkCore;
+
 namespace ProductManagementAPI
 {
     public class Program
@@ -8,6 +10,9 @@ namespace ProductManagementAPI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            // Add services to the container.
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
