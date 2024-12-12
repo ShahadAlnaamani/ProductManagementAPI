1c71a731d358a08e7fe9b4273b20db9d648d320e
Shahad <Shahadalnaamani@outlook.com>
2024-12-12 13:05:20 +0400

    Added product repo

---
+++ Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using ProductManagementAPI.Models;
+
+namespace ProductManagementAPI.Repositories
+{
+    public interface IProductRepository
+    {
+        int AddProduct(Product product);
+        void DeleteProduct(int id);
+        IEnumerable<Product> GetAllProducts();
+        Product GetProductById(int id);
+        void UpdateProduct(int id, Product product);
+    }
+}
---
+++ Repositories/ProductRepository.cs
@@ -0,0 +1,55 @@
+﻿using ProductManagementAPI.Models;
+
+namespace ProductManagementAPI.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _context.Products.ToList();
+        }
+
+        public Product GetProductById(int id)
+        {
+            return _context.Products.FirstOrDefault(p => p.PID == id);
+        }
+
+        public int AddProduct(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return product.PID;
+        }
+
+        public void DeleteProduct(int id)
+        {
+            var product = GetProductById(id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                _context.SaveChanges();
+            }
+        }
+
+        public void UpdateProduct(int id, Product product)
+        {
+            var currentProduct = GetProductById(id);
+            if (currentProduct != null)
+            {
+                currentProduct.Name = product.Name;
+                currentProduct.Price = product.Price;
+                currentProduct.Category = product.Category;
+
+                _context.Products.Update(currentProduct);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
